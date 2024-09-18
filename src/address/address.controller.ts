70434e66a9346998a06c2954b53af4196dfd3729
João Souza <ms.joao.felipe@gmail.com>
2024-09-18 10:25:17 -0300

    feat(address): add address module implementation

---
+++ src/address/address.controller.ts
@@ -0,0 +1,28 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateAddressDto } from './dtos/createAddress.dto';
+import { AddressService } from './address.service';
+import { AddressEntity } from './entities/address.entity';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enums/user-type.enum';
+import { UserId } from 'src/decorators/user-id.decorator';
+
+@Controller('address')
+export class AddressController {
+  constructor(private readonly addressService: AddressService) {}
+
+  @Roles(UserType.User)
+  @Post()
+  @UsePipes(ValidationPipe)
+  async createAddress(
+    @Body() createAddressDto: CreateAddressDto,
+    @UserId() userId: number,
+  ): Promise<AddressEntity> {
+    return this.addressService.createAddress(createAddressDto, userId);
+  }
+}
