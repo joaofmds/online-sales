96a3654bf9d5abd47243d96165c6a51c1f3f4037
João Souza <ms.joao.felipe@gmail.com>
2024-09-18 10:25:38 -0300

    feat(city): add city module implementation

---
+++ src/city/city.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { CityService } from './city.service';
+import { CityController } from './city.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityEntity } from './entities/city.entity';
+import { CacheModule } from 'src/cache/cache.module';
+
+@Module({
+  imports: [CacheModule, TypeOrmModule.forFeature([CityEntity])],
+  providers: [CityService],
+  controllers: [CityController],
+  exports: [CityService],
+})
+export class CityModule {}
