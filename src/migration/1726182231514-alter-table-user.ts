8479083f18e5ca559615d2fee5b4f7466e5e8951
João Souza <ms.joao.felipe@gmail.com>
2024-09-18 10:26:36 -0300

    feat(migration): add typeorm migrations

---
+++ src/migration/1726182231514-alter-table-user.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AlterTableUser1726182231514 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+            ALTER TABLE public.user ADD UNIQUE(email);
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+            
+        `);
+  }
+}
